b8c2f24b0a44b0a481af66a40d256ed7dc01b426
D-Niermann <dario.niermann@gmail.com>
2022-01-29 20:13:28 +0100

    styles

--- src/gridRenderer.tsx
+++ src/gridRenderer.tsx
@@ -22,7 +22,7 @@ export class GridRenderer extends React.Component<IProps, IState>{
 
 	render(){
 		return(
-			<div style={{maxWidth:2000, padding: 20, display : "grid", gridTemplateColumns: "repeat(auto-fill, minmax(400px, 1fr))", gap: 20}}>
+			<div style={{maxWidth:2000, padding: 20, display : "grid", gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))", gap: 20}}>
 				<div style = {{color: "white", backgroundColor: "black", borderRadius: 4, padding: 10}}> One </div>
 				<div style = {{color: "white", backgroundColor: "black", borderRadius: 4, padding: 10}}> Two </div>
 				<div style = {{color: "white", backgroundColor: "black", borderRadius: 4, padding: 10}}> Three </div>
